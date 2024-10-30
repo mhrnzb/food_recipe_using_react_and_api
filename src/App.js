4d45988ce39353c083c2056e14a3b44884adeb16
mhrnzb <mehrnazbg1381@gmail.com>
2024-10-30 11:44:17 +0330

    project done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import { BrowserRouter,Route,Routes } from 'react-router-dom';
+//pages
+import HomePage from './pages/HomePage/HomePage';
+import ErrorPage from './pages/ErrorPage/ErrorPage';
+import MealDetailsPage from './pages/MealDetailsPage/MealDetailsPage';
+import CategoryPage from './pages/CategoryPage/CategoryPage';
+
+//components
+import Header from "./components/Header/Header";
+// import Sidebar from "./components/Sidebar/Sidebar";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Header />
+      {/* <Sidebar /> */}
+      <Routes>
+        <Route path = "/" element = {<HomePage />} />
+        <Route path = "/meal/:id" element = {<MealDetailsPage />} />
+        <Route path = "/meal/category/:name" element = {<CategoryPage />} />
+        <Route path  = "*" element = {<ErrorPage />} />
+      </Routes>
+    </BrowserRouter>
+    
   );
 }
 
---
+++ src/pages/index.js
@@ -0,0 +1,6 @@
+import HomePage from "../pages/HomePage/HomePage.js";
+import ErrorPage from "../pages/ErrorPage/ErrorPage.js";
+import MealDetailsPage from "../pages/MealDetailsPage/MealDetailsPage.js";
+import Category from "../pages/CategoryPage/CategoryPage.js";
+
+export {HomePage,ErrorPage,MealDetailsPage,Category};
